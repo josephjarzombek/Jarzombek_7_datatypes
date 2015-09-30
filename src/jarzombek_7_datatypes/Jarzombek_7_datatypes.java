8763444737380d2e1de602e6c36e25a82fd69efc
josepjarzombek <josephjarzombek@ninja20.d22.lan>
2015-09-30 10:41:57 -0500

    Jarzombek datatypes

---
+++ src/jarzombek_7_datatypes/Jarzombek_7_datatypes.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jarzombek_7_datatypes;
+
+/**
+ *
+ * @author josephjarzombek
+ */
+public class Jarzombek_7_datatypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // code explanation is an exspectation
+        int i = 1;
+        float a = 2.5f;
+        double b = 3.149;
+        short c = 16;
+        char d1 =  ';';
+        char d2 = 'y';
+        char d3 = 42;
+        char d4 = 145;
+        String e =d4+"";
+        String e1 = "420 blaze it";
+        boolean f = true;
+        boolean g = i % 2 == 0;
+        /*
+        here we will loo at some mathmatical operators
+        */
+        // + adds two numbers
+        // - subtracts two numbers
+        // * multiplies two numbers
+        // / divides two numbers, more
+        // % this is a modulo shows remainders. i % 2 checks if a number is even or odd.
+//        System.out.println(i + " an interager");
+//        System.out.println(a + " a float");
+//        System.out.println(b + " a double");
+//        System.out.println(c + " a short");
+//        System.out.println(d1 + " a char");
+//        System.out.println(d2 + " a char");
+//        System.out.println(d3 + " a char");
+//        System.out.println(e + " a string");
+//        System.out.println(f + " a boolean");
+        //for(i = -153;i < 153; i++){
+           // System.out.println((char) i);
+            //System.out.println(i % 3 == 0);
+//            i = 22;
+//            a = 0;
+//            e = "ninja";
+//            System.out.println(i + e + " sting plus an interger ");
+            //automatically up casted to a float. 7 + 0.0 = 7.0
+            // adding an int and a string cncatenates the int and string as a string
+            for (i = 1; i < 101; i++) {
+                //if is a conditional (flow) statement. If a condition is true the following command will be executed
+            if (for == 0) {
+                System.out.println(i + " is even");
+            } else {
+                System.out.println(i + " is odd");
+            }
+        }
+    }
+    
+}
